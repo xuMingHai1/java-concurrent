33fa152cb40895e42e399f76688d652850dfdfc6
徐明海 <173535609@qq.com>
2022-05-07 00:00:17 +0800

    Logback 配置修改

--- src/main/java/xyz/xuminghai/logback/LogbackConfig.java
+++ src/main/java/xyz/xuminghai/logback/LogbackConfig.java
@@ -11,7 +11,7 @@ import ch.qos.logback.core.spi.ContextAwareBase;
 
 /**
  * 2022/3/1 20:49 星期二<br/>
- * logback配置
+ * 基于SPI的logback配置
  * @author xuMingHai
  */
 public class LogbackConfig extends ContextAwareBase implements Configurator {
@@ -24,7 +24,7 @@ public class LogbackConfig extends ContextAwareBase implements Configurator {
         final ConsoleAppender<ILoggingEvent> consoleAppender = new ConsoleAppender<>();
         // 设置上下文
         consoleAppender.setContext(loggerContext);
-        // 设置顶级附加器名字
+        // 设置附加器名字
         consoleAppender.setName("root");
 
         // 字符集
@@ -35,14 +35,20 @@ public class LogbackConfig extends ContextAwareBase implements Configurator {
         final PatternLayout patternLayout = new PatternLayout();
         patternLayout.setContext(loggerContext);
         patternLayout.setPattern("%d %highlight(%-5level) [%thread] --- %cyan(%logger{25}) : %msg %n");
-        // 开始配置
+        // 激活布局（完成设置）
         patternLayout.start();
 
+        // 编码设置布局
         encoder.setLayout(patternLayout);
+        // 激活编码
+        encoder.start();
 
+        // 控制台附加器设置编码
         consoleAppender.setEncoder(encoder);
+        // 激活此附加器
         consoleAppender.start();
 
+        // 在root记录器添加附加器
         loggerContext.getLogger(Logger.ROOT_LOGGER_NAME).addAppender(consoleAppender);
     }
 }
